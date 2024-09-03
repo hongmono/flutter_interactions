4b119ce1a1a0cdc822479683b5f3da2768bb85dd
hongmono <hojw1019@gmail.com>
2024-09-03 11:19:26 +0900

    feat: create example

---
+++ packages/burst_icon_button/burst_button_example/android/app/src/main/kotlin/com/example/burst_button_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.burst_button_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ packages/burst_icon_button/burst_button_example/lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:burst_icon_button/burst_icon_button.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BurstIconButton',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const App(title: 'BurstIconButton'),
+    );
+  }
+}
+
+class App extends StatefulWidget {
+  const App({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> with TickerProviderStateMixin {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: BurstIconButton(
+          icon: const Icon(Icons.favorite_border),
+          pressedIcon: const Icon(Icons.favorite),
+          burstIcon: const Icon(Icons.heart_broken),
+          onPressed: () {},
+        ),
+      ),
+    );
+  }
+}
