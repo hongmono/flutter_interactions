2838ea529edfec1a7531d0168db95b62a93c67c0
hongmono <hojw1019@gmail.com>
2024-09-10 22:44:14 +0900

    feat: floating widget

--- packages/floating_widget/floating_widget_example/lib/main.dart
+++ packages/floating_widget/floating_widget_example/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:floating_widget/floating_widget.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -40,29 +41,31 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
+    return FloatingWidget(
+      padding: const EdgeInsets.all(16),
+      floatingWidget: FloatingActionButton(
         onPressed: _incrementCounter,
-        tooltip: 'Increment',
         child: const Icon(Icons.add),
+      ),
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'You have pushed the button this many times:',
+              ),
+              Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
