d890799b8c0976589951b3c55a5017bcace98087
hongmono <hojw1019@gmail.com>
2024-09-28 23:05:17 +0900

    feat: 순서 바꾸는 로직 추가

--- apps/card_cascade/lib/main.dart
+++ apps/card_cascade/lib/main.dart
@@ -7,27 +7,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -39,15 +23,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -55,71 +30,193 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: const Center(
+        child: SizedBox(
+          width: 300,
+          height: 200,
+          child: CardCascade(),
+        ),
+      ),
+    );
+  }
+}
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+class CardCascade extends StatefulWidget {
+  const CardCascade({super.key});
+
+  @override
+  State<CardCascade> createState() => _CardCascadeState();
+}
+
+class _CardCascadeState extends State<CardCascade> {
+  late List<CardData> cards;
+
+  @override
+  void initState() {
+    super.initState();
+
+    cards = [
+      CardData(key: GlobalKey(), color: Colors.red),
+      CardData(key: GlobalKey(), color: Colors.green),
+      CardData(key: GlobalKey(), color: Colors.blue),
+      CardData(key: GlobalKey(), color: Colors.yellow),
+      CardData(key: GlobalKey(), color: Colors.purple),
+    ];
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return Stack(
+      children: [
+        ...cards.asMap().entries.map(
+              (entry) => Card(
+                key: entry.value.key,
+                index: entry.key,
+                color: entry.value.color,
+                onTap: _onCardSwipe,
+              ),
+            )
+      ],
+    );
+  }
+
+  void _onCardSwipe(bool? isRight) {
+    if (isRight == true) {
+      setState(() {
+        final temp = cards.removeLast();
+
+        cards = [temp, ...cards];
+      });
+    }
+  }
+}
+
+class CardData {
+  const CardData({required this.key, required this.color});
+
+  final GlobalKey key;
+  final Color color;
+}
+
+class Card extends StatefulWidget {
+  const Card({
+    super.key,
+    required this.index,
+    required this.color,
+    this.onCardSwipe,
+    this.onTap,
+  });
+
+  final int index;
+  final Color color;
+  final Function(bool?)? onCardSwipe;
+  final Function(bool?)? onTap;
+
+  @override
+  State<Card> createState() => _CardState();
+}
+
+class _CardState extends State<Card> with TickerProviderStateMixin {
+  late Offset _initialPosition;
+  late Offset _position;
+
+  late final AnimationController _controller = AnimationController(
+    vsync: this,
+    duration: const Duration(milliseconds: 1000),
+  );
+
+  Animation<Offset>? _animation;
+
+  late final AnimationController _zIndexController = AnimationController(
+    vsync: this,
+    duration: const Duration(milliseconds: 300),
+  );
+
+  Animation<double>? _zIndexAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _initialPosition = Offset.zero;
+    _position = _initialPosition;
+
+    _controller.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _animation = null;
+      }
+    });
+  }
+
+  @override
+  void didUpdateWidget(covariant Card oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    if (widget.index != oldWidget.index) {
+      _zIndexAnimation = Tween(
+        begin: oldWidget.index.toDouble(),
+        end: widget.index.toDouble(),
+      ).animate(_zIndexController);
+
+      _initialPosition = Offset.zero;
+      _position = _initialPosition;
+
+      _zIndexController.forward(from: 0);
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    _controller.dispose();
+    _zIndexController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _zIndexController,
+      builder: (context, child) {
+        double index = _zIndexAnimation?.value.toDouble() ?? widget.index.toDouble();
+
+        return Transform(
+          transform: Matrix4.translationValues(-index * 16 + _position.dx, index * 8 + _position.dy, 0),
+          alignment: FractionalOffset.center,
+          child: GestureDetector(
+            onTap: () {
+              if (_zIndexController.isAnimating) return;
+              widget.onTap?.call(true);
+            },
+            onPanUpdate: (details) {
+              setState(() {
+                _position += details.delta;
+              });
+            },
+            onPanEnd: (details) {},
+            child: Container(
+              decoration: BoxDecoration(
+                color: widget.color,
+                borderRadius: BorderRadius.circular(16),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.5),
+                    blurRadius: 8,
+                    offset: const Offset(0, 2),
+                  ),
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          ),
+        );
+      },
     );
   }
 }
